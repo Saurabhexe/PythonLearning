3602cc4838b58815fd6282acddfb17db5a8a2b52
saurabhexe <saurabhtayde29@gmail.com>
2022-08-10 23:47:49 +0530

    funtion in python

---
+++ 06_Funtions/funtion.py
@@ -0,0 +1,12 @@
+# A simple Python function
+
+def fun(text):
+    print("Welcome to GFG")
+
+# A simple Python function
+def fun():
+	print("Welcome to GFG")
+
+
+# Driver code to call a function
+fun()
