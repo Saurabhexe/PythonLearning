d031a22b0c44ee678908d1b0300c4a675ae2b1f5
saurabhexe <saurabhtayde29@gmail.com>
2022-08-10 23:53:54 +0530

    modified:   06_Funtions/funtion.py

--- 06_Funtions/funtion.py
+++ 06_Funtions/funtion.py
@@ -10,3 +10,16 @@ def fun():
 
 # Driver code to call a function
 fun()
+# _________________________________________________
+
+def add(num1: int, num2: int) -> int:
+	"""Add two numbers"""
+	num3 = num1 + num2
+
+	return num3
+
+# Driver code
+num1, num2 = 5, 15
+ans = add(num1, num2)
+print(f"The addition of {num1} and {num2} results {ans}.")
+
